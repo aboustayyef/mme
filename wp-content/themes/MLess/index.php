80e1deb1b82deae1fb9b675a90e6b5bd349dc9b5
Digital Ocean Server <mustapha.hamoui@gmail.com>
2014-03-21 05:35:40 +0000

    added MLess

---
+++ wp-content/themes/MLess/index.php
@@ -0,0 +1,272 @@
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+<link rel="shortcut icon" href="http://beirutspring.com/favicon.ico?v=001" />
+<title><?php bloginfo('name'); ?> | <?php if( is_home() ) : echo bloginfo( 'description' ); endif; ?><?php wp_title( '', true ); ?></title>
+
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start header
+	/*-----------------------------------------------------------------------------------*/
+?>
+
+<header id="masthead" class="site-header" role="banner">
+	<div class="container">
+		
+		<div class="gravatar">
+			<?php 
+				// grab admin email and their photo
+				//$admin_email = get_option('admin_email');
+				//echo get_avatar( $admin_email, 100 ); 
+			?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<img src ="http://mustapha.me/mustapha-me-logo.png" width ="150px" height ="auto" alt ="Beirut Spring logo">
+			</a>
+		</div><!--/ author -->
+		
+		<div id="brand">
+			<h1 class="site-title"><span><?php echo get_bloginfo( 'description' ); ?></span></h1>
+			<nav role="navigation" class="site-navigation main-navigation">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav><!-- .site-navigation .main-navigation -->
+		</div><!-- /brand -->
+	
+		<div class="clear"></div>
+	</div><!--/container -->
+		
+</header><!-- #masthead .site-header -->
+
+<div class="container">
+
+	<div id="primary">
+		<div id="content" role="main">
+
+
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Home loop
+	/*-----------------------------------------------------------------------------------*/
+	
+	if( is_home() || is_archive() ) {
+	
+?>
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article class="post">
+					
+						<h1 class="title">
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+								<?php the_title() ?>
+							</a>
+						</h1>
+						<div class="post-meta">
+							<?php if( comments_open() ) : ?>
+								<span class="comments-link">
+									<?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
+								</span>
+							<?php endif; ?>
+						
+						</div><!--/post-meta -->
+						
+						<div class="the-content">
+							<?php the_content( 'Continue...' ); ?>
+							
+							<?php wp_link_pages(); ?>
+						</div><!-- the-content -->
+						
+						<div class="meta clearfix">
+							<!--<div class="category"><?php echo get_the_category_list(); ?></div>-->
+							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
+						</div><!-- Meta -->
+						
+					</article>
+
+				<?php endwhile; ?>
+				
+				<!-- pagintation -->
+				<div id="pagination" class="clearfix">
+					<div class="past-page"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
+					<div class="next-page"><?php next_posts_link( ' &laquo; Older' ); ?></div>
+				</div><!-- pagination -->
+
+
+			<?php else : ?>
+				
+				<article class="post error">
+					<h1 class="404">Nothing posted yet</h1>
+				</article>
+
+			<?php endif; ?>
+
+		
+	<?php } //end is_home(); ?>
+
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Single loop
+	/*-----------------------------------------------------------------------------------*/
+	
+	if( is_single() ) {
+?>
+
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article class="post">
+					
+						<h1 class="title"><?php the_title() ?></h1>
+						<div class="post-meta">
+							<?php if( comments_open() ) : ?>
+								<span class="comments-link">
+									<?php comments_popup_link( __( 'Comment', 'less' ), __( '1 Comment', 'less' ), __( '% Comments', 'less' ) ); ?>
+								</span>
+							<?php endif; ?>
+						
+						</div><!--/post-meta -->
+						
+						<div class="the-content">
+							<?php the_content( 'Continue...' ); ?>
+							
+							<?php wp_link_pages(); ?>
+						</div><!-- the-content -->
+						
+						<div class="meta clearfix">
+							<div class="category"><?php echo get_the_category_list(); ?></div>
+							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
+						</div><!-- Meta -->						
+	
+						<div>
+							<hr>
+							<p>Do you like this post? consider following its author on <a href ="http://twitter.com/beirutspring">twitter</a> </p>
+						</div>
+					
+					</article>
+				<?php endwhile; ?>
+				
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() )
+						comments_template( '', true );
+				?>
+
+
+			<?php else : ?>
+				
+				<article class="post error">
+					<h1 class="404">Nothing posted yet</h1>
+				</article>
+
+			<?php endif; ?>
+
+
+	<?php } //end is_single(); ?>
+	
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Page loop
+	/*-----------------------------------------------------------------------------------*/
+	
+	if( is_page()) {
+?>
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article class="post">
+					
+						<h1 class="title"><?php the_title() ?></h1>
+						
+						<div class="the-content">
+							<?php the_content(); ?>
+							
+							<?php wp_link_pages(); ?>
+						</div><!-- the-content -->
+						
+					</article>
+
+				<?php endwhile; ?>
+
+			<?php else : ?>
+				
+				<article class="post error">
+					<h1 class="404">Nothing posted yet</h1>
+				</article>
+
+			<?php endif; ?>
+
+	<?php } // end is_page(); ?>
+
+		</div><!-- #content .site-content -->
+	</div><!-- #primary .content-area -->
+
+</div><!-- / container-->
+
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Footer
+	/*-----------------------------------------------------------------------------------*/
+?>
+
+<footer class="site-footer" role="contentinfo">
+	<div class="site-info container">
+		<?php do_action( 'break_credits' ); ?>
+		<a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
+	</div><!-- .site-info -->
+</footer><!-- #colophon .site-footer -->
+
+<?php wp_footer(); ?>
+
+<!-- Start of StatCounter Code -->
+<script type="text/javascript">
+var sc_project=3313987; 
+var sc_invisible=1; 
+var sc_security="eb0f8910"; 
+</script>
+<script type="text/javascript"
+src="http://www.statcounter.com/counter/counter.js"></script>
+<noscript><div class="statcounter"><a title="site stats"
+href="http://statcounter.com/free-web-stats/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/3313987/0/eb0f8910/1/"
+alt="site stats"></a></div></noscript>
+
+<!-- End of StatCounter Code -->
+
+<!-- Start of Google Analytics Code -->
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-16792545-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+<!-- End of Google Analytics Code -->
+
+
+</body>
+</html>
